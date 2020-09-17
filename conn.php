661bc818f1e3fccb713e8a84900bdbe6e9768dab
Michel <>
2020-09-17 20:34:09 +0200

    use dynamic route, environment variables, no caps and no underscores

--- conn.php
+++ conn.php
@@ -1,10 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$host = $_ENV['DB_HOST'];
+$connection = $_ENV['DB_CONNECTION'];
+$databaseName = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=kok", $username, $password);
+  $conn = new PDO("$connection:host=$host;dbname=$databaseName", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
--- index.php
+++ index.php
@@ -4,43 +4,57 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Get full url
+$fullUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+/**
+ * php function to parse all parts of an url
+ */
+$parsed_url = parse_url($fullUrl);
+
+$trim_path = preg_replace('/^\/+/', '', $parsed_url['path']);
+$path_array = explode(DIRECTORY_SEPARATOR, $trim_path);
+
+// Quick fix for possibility to run from a subfolder
+$basePath = '/';
+if($path_array[0] === $_ENV['BASE_PATH']) {
+	$basePath = array_shift($path_array);
+}
+
+
 include "navbar.html";
 include "conn.php";
 
-
-$url = $_SERVER['REQUEST_URI'];
-
-
-if($url == '/praatmetdekoktest/Voorgerecht_aanmaken') {
+if($path_array[0] == 'voorgerecht-aanmaken') {
     include "vooraanmaken.html";
     include "vooraanmaken.php";
 }
 
-
-if($url == '/praatmetdekoktest/Hoofdgerecht_aanmaken') {
+var_dump($path_array);
+if($path_array[0] == 'hoofdgerecht-aanmaken') {
     include "hoofdaanmaken.html";
     include "hoofdaanmaken.php";
 }
 
 
-if($url == '/praatmetdekoktest/Nagerecht_aanmaken') {
+if($path_array[0] == 'nagerecht-aanmaken') {
     include "naaanmaken.html";
     include "naaanmaken.php";
 }
 
 
-if($url == '/praatmetdekoktest/Bijgerecht_aanmaken') {
+if($path_array[0] == 'bijgerecht-aanmaken') {
     include "bijaanmaken.html";
     include "bijaanmaken.php";
 }
 
 
-if($url == '/praatmetdekoktest/Saus_aanmaken') {
+if($path_array[0] == 'saus-aanmaken') {
     include "sausaanmaken.html";
     include "sausaanmaken.php";
 }
 
-if($url == '/praatmetdekoktest/Menu_inzien') {
+if($path_array[0] == 'menu-inzien') {
     include "menueditform.php";
     include "menueditdefinitief.php";
     include "deletegerecht.php";
