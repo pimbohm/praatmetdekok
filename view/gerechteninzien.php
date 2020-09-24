8a0c6e3b773b65e8cbc0c84b5f6089e5decb46e7
michelt81 <micheltolner@gmail.com>
2020-09-24 20:11:19 +0200

    ternary operator

--- view/gerechteninzien.php
+++ view/gerechteninzien.php
@@ -1,22 +1,15 @@
-
 <h1>Gerechten</h1>
 <?php foreach($dish as $d) { ?>
         <form method="post">
             <input type="hidden" name="id" value="<?= $d['id']; ?>">
             <?php
-            echo 'Gerecht: ' . $d['name'];
-            echo '<br>';
-            echo 'Beschrijving: ' . $d['description'];
-            echo '<br>';
-            echo 'type: ' . $d['type'];
-            echo '<br>';
-            echo 'Aangemaakt op: ' . $d['created_at'];
-            echo '<br>';
-            if ($d['updated_at'] != '') {
-                echo 'Gewijzigd op: ' . $d['updated_at'];
-                echo '<br>';
-            }
+            echo 'Gerecht: ' . $d['name'] . '<br>';
+            echo 'Beschrijving: ' . $d['description'] . '<br>';
+            echo 'type: ' . $d['type'] . '<br>';
+            echo 'Aangemaakt op: ' . $d['created_at'] . '<br>';
+            echo (!empty($d['updated_at']) ? 'Gewijzigd op: ' . $d['updated_at'] . '<br>' : '');
             ?>
+                <br>
                 <input type="submit" name="edit" value="wijzigen">
                 <input type="submit" name="delete" value="verwijderen">
                 <br>
